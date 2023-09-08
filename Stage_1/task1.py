003a36810d78a1120f32f1021afeb6da29f90f64
phurhard <phurhardeen@gmail.com>
2023-09-08 02:47:49 +0100

    pep8

--- Stage_1/task1.py
+++ Stage_1/task1.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-from flask import Flask, jsonify,request, Blueprint
+from flask import Flask, jsonify, request, Blueprint
 from os import getenv
 from datetime import timedelta, datetime
 
@@ -7,6 +7,8 @@ from datetime import timedelta, datetime
 app = Flask(__name__)
 app_views = Blueprint("app_views", __name__, url_prefix="")
 app.json.sort_keys = False
+
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
@@ -22,20 +24,30 @@ def stats() -> str:
     Return:
       - the parameters of the user and track
     """
-    print(request.args)
 
     name = request.args.get('slack_name')
-    day = datetime.now().strftime("%A")
+    day = datetime.utcnow().strftime("%A")
     time = datetime.utcnow()
     current_time = time.strftime('%Y-%m-%dT%H:%M:%SZ')
     Track = request.args.get('track')
-    github_url_file = 'https://github.com/phurhard/HNGx/blob/main/Stage_1/task1.py'
-    github_url_repo = 'https://github.com/phurhard/HNGx/tree/main'
+    github_url_file = 'https://github.com/
+    phurhard/HNGx/blob/main/Stage_1/task1.py'
+    github_url_repo = 'https://github.com/
+    phurhard/HNGx/tree/main'
     status_code = 200
-    stats = {'slack_name': name, 'current_day':day, 'utc_time': current_time, 'track': Track, 'github_file_url':github_url_file, 'github_repo_url':github_url_repo, 'status_code': status_code}
+    stats = {'slack_name': name,
+             'current_day': day,
+             'utc_time': current_time,
+             'track': Track,
+             'github_file_url': github_url_file,
+             'github_repo_url': github_url_repo,
+             'status_code': status_code
+             }
     return jsonify(stats)
 
+
 app.register_blueprint(app_views)
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
